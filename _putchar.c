ff18b43de00e8d008a7a4eed88ba094291c7c2cc
Bello Emmanuel Oluwatimilehin <59453152+Timilehin-bello@users.noreply.github.com>
2022-04-20 09:35:10 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,4 +10,19 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register short i;
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	return (i);
 }
